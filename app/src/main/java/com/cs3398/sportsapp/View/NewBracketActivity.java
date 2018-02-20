c4e07682b3c060820e29ed60f95f406532a9f0a8
Jeffrey Jang <j_j264@users.noreply.github.com>
2018-02-20 13:48:29 -0600

    Updated New Bracket Activity and GUI

--- app/src/main/java/com/cs3398/sportsapp/View/NewBracketActivity.java
+++ app/src/main/java/com/cs3398/sportsapp/View/NewBracketActivity.java
@@ -2,19 +2,31 @@ package com.cs3398.sportsapp.View;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.cs3398.sportsapp.R;
 
-/**
- * Created by Jjang on 2/20/2018.
- */
 
 public class NewBracketActivity extends AppCompatActivity {
+    private Button done;
+    private EditText name1, name2, name3, name4, name5, name6, name7, name8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newbracket);
+
+        name1 = (EditText)findViewById(R.id.name1);
+        name2 = (EditText)findViewById(R.id.name2);
+        name3 = (EditText)findViewById(R.id.name3);
+        name4 = (EditText)findViewById(R.id.name4);
+        name5 = (EditText)findViewById(R.id.name5);
+        name6 = (EditText)findViewById(R.id.name6);
+        name7 = (EditText)findViewById(R.id.name7);
+        name8 = (EditText)findViewById(R.id.name8);
+
+        done = (Button)findViewById(R.id.addPlayerDone);
 
 
     }
