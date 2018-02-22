af63a038d54a5e16f0fc51d07094cad53b051575
Mason M <mpm87@txstate.edu>
2018-02-21 22:32:18 -0600

    Changed initial database class

---
+++ app/src/main/java/com/cs3398/sportsapp/Model/DBHandler.java
@@ -0,0 +1,35 @@
+package com.cs3398.sportsapp.Model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "users info";
+    // Contacts table name
+    private static final String TABLE_USERS = "users";
+    // Users Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_ADDRESS = "address";
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE" + TABLE_USERS + "("
+        + KEY_ID + "INTEGER PRIMARY KEY," + KEY_NAME + "TEXT,"
+        + KEY_ADDRESS + "TEXT" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_USERS);
+    // Creating tables again
+        onCreate(db);
+    }
+}
