e058dfcd6cd3b489246e78e1c4eee3da6f2a80cd
Mason M <mpm87@txstate.edu>
2018-02-21 23:06:46 -0600

    DBHandler master class handles the database. Should be complete once identifiers for user class are updated

--- app/src/main/java/com/cs3398/sportsapp/Model/DBHandler.java
+++ app/src/main/java/com/cs3398/sportsapp/Model/DBHandler.java
@@ -1,8 +1,12 @@
 package com.cs3398.sportsapp.Model;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHandler extends SQLiteOpenHelper {
     // Database Version
@@ -31,5 +35,76 @@ public class DBHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS" + TABLE_USERS);
     // Creating tables again
         onCreate(db);
+    }
+
+    // Adding new shop
+    public void addUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, user.getLastName()); // user Name
+        values.put(KEY_ADDRESS, user.getSportsPreference()); // user preference
+    // Inserting Row
+        db.insert(TABLE_USERS, null, values);
+        db.close(); // Closing database connection
+    }
+    // Getting one user
+    public User getUser(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS, new String[]{KEY_ID,
+                KEY_NAME, KEY_ADDRESS}, KEY_ID + "=?",
+        new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+        User contact = new User(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+// return shop
+        return contact;
+    }
+    // Getting All Shops
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<User>();
+// Select All Query
+        String selectQuery = "SELECT * FROM" + TABLE_USERS;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+// looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                User user = new User();
+                user.setId(Integer.parseInt(cursor.getString(0)));
+                user.setName(cursor.getString(1));
+                user.setAddress(cursor.getString(2));
+// Adding contact to list
+                userList.add(user);
+            } while (cursor.moveToNext());
+        }
+// return contact list
+        return userList;
+    }
+    // Getting shops Count
+    public int getUsersCount() {
+        String countQuery = "SELECT * FROM" + TABLE_USERS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+// return count
+        return cursor.getCount();
+    }
+    // Updating a shop
+    public int updateUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, user.getName());
+        values.put(KEY_ADDRESS, user.getAddress());
+// updating row
+        return db.update(TABLE_USERS, values, KEY_ID + "= ?",
+        new String[]{String.valueOf(user.getId())});
+    }
+    // Deleting a user
+    public void deleteUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_USERS, KEY_ID + "= ?",
+        new String[] { String.valueOf(user.getId()) });
+        db.close();
     }
 }
